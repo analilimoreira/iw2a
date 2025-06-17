bed9e8bd2b67fabeac43cf46b34d1a2bb604f7d2
Aluno.Etec <aula.etec.com>
2025-06-17 15:58:22 -0300

    fdgrdtg

---
+++ pokedex/script.js
@@ -0,0 +1,50 @@
+let currentId = 1;
+
+function searchPokemon() {
+  const input = document.getElementById("search").value.toLowerCase();
+  if (!input) return;
+
+  fetchPokemon(input);
+}
+
+function prevPokemon() {
+  if (currentId > 1) {
+    currentId--;
+    fetchPokemon(currentId);
+  }
+}
+
+function nextPokemon() {
+  currentId++;
+  fetchPokemon(currentId);
+}
+
+function fetchPokemon(identifier) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`)
+    .then(res => {
+      if (!res.ok) throw new Error("Pokémon não encontrado");
+      return res.json();
+    })
+    .then(data => {
+      currentId = data.id;
+      document.getElementById("poke-name").textContent = data.name.toUpperCase();
+      document.getElementById("poke-id").textContent = data.id;
+      document.getElementById("poke-img").src = data.sprites.other["official-artwork"].front_default;
+
+      const typesContainer = document.getElementById("poke-types");
+      typesContainer.innerHTML = "";
+
+      data.types.forEach(t => {
+        const span = document.createElement("span");
+        span.classList.add("type", t.type.name);
+        span.textContent = t.type.name;
+        typesContainer.appendChild(span);
+      });
+    })
+    .catch(err => {
+      alert("Pokémon não encontrado!");
+    });
+}
+
+// Carrega o primeiro Pokémon (Bulbasaur)
+fetchPokemon(currentId);
